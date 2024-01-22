4393219ece37ef19e0cf5bdaabcf0fb58ba7cb3d
Rakshit <rakshit25082001@gmail.com>
2024-01-23 00:54:30 +0530

    console logs are used to know fetched data format now removing all coonsloe.logs

--- OneDrive/Desktop/netflix-gpt/src/hooks/useMovieTrailer.js
+++ OneDrive/Desktop/netflix-gpt/src/hooks/useMovieTrailer.js
@@ -1,9 +1,14 @@
-const useMovieTrailer = () => 
+import { useDispatch } from "react-redux";
+import { addTrailerVideo } from "../Utils/moviesSlice";
+import { useEffect } from "react";
+import {API_OPTIONS} from "../Utils/constants"
+
+const useMovieTrailer = (movieId) => 
 {
-    const dispatch = useDispatch();
+     const dispatch = useDispatch();
 
      const getMovieVideos = async () => {
-      const data = await fetch('https://api.themoviedb.org/3/movie/976573/videos?language=en-US', API_OPTIONS);
+      const data = await fetch('https://api.themoviedb.org/3/movie/' + movieId +'/videos?language=en-US', API_OPTIONS);
       const json = await data.json();
       console.log(json);
 
